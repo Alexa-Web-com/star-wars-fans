34ddb08c810143289b2ee2f875290af9c70f8bd6
oleleiNET <115529700+oleleiNET@users.noreply.github.com>
2023-04-13 11:06:18 +0200

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 import './Footer.css'
+import logoAlexaWeb from '../../assets/logoAlexaWeb.png'
 
 const Footer = () => {
     return (
-        <small className='footer'>© 2023 Alexa-Web development. All rights reserved.</small>
+        <small className='footer'>
+            <p>Copyright © 2023 </p>
+
+            <a href='https://alexa-web.com'
+                target='_blank'
+                rel='noreferrer'
+                className='footer_link'
+            >
+                <img src={logoAlexaWeb} alt='owner logo' className='footer_logo' />
+                <p>Alexa-Web.com</p>
+            </a>
+        </small>
+
     )
 }
 export default Footer
